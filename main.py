a70539333211f5d86e011bee6d4b4a9b1c1589d6
sofochkalivenskaya <92178996+sofochkalivenskaya@users.noreply.github.com>
2021-10-14 23:06:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:54:25) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> a = 'Hello world'
+>>> print(a)
+Hello world
+>>> 
